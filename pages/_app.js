81d4bb9728eff0d8dfdec99def3564696dee57de
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2023-05-14 02:11:12 +0200

    [+] ga4

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 import { appWithTranslation } from 'next-i18next';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
@@ -17,10 +18,24 @@ function MyApp ({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <Head>
-        <title>Dokkan Battle Battle </title>
+        <title>Dokkan Battle Battle - Fight your friends in multiplayer game mode</title>
+        <meta name="description" content="Dokkan Battle Battle allows you to fight your friends on Dokkan Battle in different game modes"></meta>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
+
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
 
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
