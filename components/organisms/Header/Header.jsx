6c8906bdd9922e7912debe36d7db76f3c3234843
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2023-05-14 02:31:18 +0200

    [*] disabled link

--- components/organisms/Header/Header.jsx
+++ components/organisms/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <div className={styles['dbb-navbar']}>
       <Link href="/patchs" className={styles['dbb-articles']}>
-        1.20
+        2.?.?
       </Link>
       <Link href="/" className={styles['dbb-navbar__logo']}>
         <div className={styles['dbb-navbar__logo--d']} />
@@ -18,10 +18,10 @@ function Header() {
         <div className={styles['dbb-navbar__logo--attle2']} />
       </Link>
       <nav className={styles['dbb-navbar__links']}>
-        <Link href="/random-rush" className={styles['dbb-navbar__link']}>
+        <Link disabled href="/random-rush" className={styles['dbb-navbar__link']}>
           Random Rush
         </Link>
-        <Link href="/challenge-battle" className={styles['dbb-navbar__link']}>
+        <Link disabled href="/challenge-battle" className={styles['dbb-navbar__link']}>
           Challenge Battle
         </Link>
       </nav>
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ function Home() {
       <div className="container center">
         <h1 className="h1">Dokkan Battle Battle</h1>
         <h2 className="h2">Site en maintenance</h2>
-        <h3 className="h3">Le site est en cours de migration de serveur</h3>
+        <h3 className="h3">Le site est en cours de migration serveur</h3>
       </div>
     </WithHeaderFooter>
   );
