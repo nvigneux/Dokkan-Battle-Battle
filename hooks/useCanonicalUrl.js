521071051f45f30df73f0563bed0aa8a8fc30f4c
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-05-15 09:14:54 +0200

    Review before push on Hooks

--- hooks/useCanonicalUrl.js
+++ hooks/useCanonicalUrl.js
@@ -3,12 +3,24 @@ import { useRouter } from 'next/router';
 // Utils
 import { DOMAINS_LOCALE } from '../utils/constants';
 
+/**
+ * Extracts the path from the given URL.
+ * @param {string} asPath - The URL to extract the path from.
+ * @returns {string} The extracted path.
+ */
+function getPath(asPath) {
+  return asPath.split('?')[0].toLowerCase();
+}
+
+/**
+ * Uses the Next.js router to get the current URL and locale,
+ * and constructs a canonical URL using the locale-specific domain.
+ * @returns {string} The canonical URL.
+ */
 function useCanonicalUrl() {
   const { asPath, locale } = useRouter();
-  const path = asPath.split('?')[0].toLowerCase();
-  return (`${DOMAINS_LOCALE[locale]}${asPath === '/'
-    ? ''
-    : path}`);
+  const path = getPath(asPath);
+  return `${DOMAINS_LOCALE[locale]}${asPath === '/' ? '' : path}`;
 }
 
 export default useCanonicalUrl;
